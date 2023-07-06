c92efa226275b5938aef9a6441db47647b4d4f0b
code-Assasin <harshitha.machiraju@epfl.ch>
2023-07-06 14:54:26 +0000

    cleaned up

--- run.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=3 python test.py --atk_type mufia --threat_model std --batch_size 32 --dataset cifar10 --n_epochs 100 --lr 0.1 --model_name resnet56 --lambda_reg 20.0 --block_size 32 --verbose --kappa 0.99
